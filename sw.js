b27894a3cd1eb0862e96e816b2bec642cc074b4a
Mikes-Bridge-Scorer <mike.chris.smith@gmail.com>
2026-05-27 09:45:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'digibid-v29';
+const CACHE = 'digibid-v30';
 const ASSETS = ['./','./digibid.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
